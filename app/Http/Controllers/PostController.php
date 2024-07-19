bd40f0668e05a26f327b34219d80e69573ce1485
progamerxml <progamerxml@outlook.com>
2024-07-19 10:33:04 +0700

    ###stoping on first validation failur

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -2,9 +2,37 @@
 
 namespace App\Http\Controllers;
 
+use Illuminate\Http\RedirectResponse;
 use Illuminate\Http\Request;
+use Illuminate\View\View;
 
 class PostController extends Controller
 {
-    //
+    public function create(): View
+    {
+        return view('post.create');
+    }
+
+    public function store(Request $request): RedirectResponse
+    {
+        // validasi standar
+        $dataValid = $request->validate([
+            'title' => 'required|unique:posts|max:255',
+            'post' => 'require'
+        ]);
+
+        // validasi sebagai array
+        $dataValid = $request->validate([
+            'title' => ['required','unique:posts','max:255'],
+            'post' => ['require'],
+        ]);
+
+        // validasi dengan method validateWithBag
+        $dataValid = $request->validateWithBag('post', [
+            'title' => 'required|unique:posts|max:255',
+            'post' => 'require'
+        ]);
+
+        return redirect('/posts');
+    }
 }
